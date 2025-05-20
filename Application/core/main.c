9af54a3888a34454cdf710308e45dbac736e6fff
Maxim <zhiharev.dev@mail.ru>
2025-05-20 11:37:21 +0300

    Инициализировать RCC

--- Application/core/main.c
+++ Application/core/main.c
@@ -23,9 +23,21 @@
 
 /* Private constants ------------------------------------------------------- */
 
-#define VTOR_ADDRESS        0x08000000
+#define VTOR_ADDRESS            0x08000000
 
-#define HSI_CLOCK           8000000
+#define HSI_CLOCK               8000000
+
+#define RCC_HSERDY_TIMEOUT      100
+
+#define RCC_LSERDY_TIMEOUT      5000
+
+#define RCC_CPU_CLOCK           72000000
+
+#define RCC_AHB_CLOCK           (RCC_CPU_CLOCK / 1)
+
+#define RCC_APB1_CLOCK          (RCC_AHB_CLOCK / 2)
+
+#define RCC_APB2_CLOCK          (RCC_AHB_CLOCK / 1)
 
 /* Private types ----------------------------------------------------------- */
 
@@ -50,6 +62,8 @@ static void systick_init(const uint32_t frequency);
 static void pwr_init(void);
 
 static void flash_init(void);
+
+static void rcc_init(void);
 
 /* Private user code ------------------------------------------------------- */
 
@@ -85,6 +99,8 @@ static void setup_hardware(void)
     systick_init(HSI_CLOCK);
     pwr_init();
     flash_init();
+    rcc_init();
+    systick_init(RCC_CPU_CLOCK);
 }
 /* ------------------------------------------------------------------------- */
 
@@ -155,6 +171,70 @@ static void flash_init(void)
     /* Включить предварительную выборку данных */
     SET_BIT(FLASH->ACR, FLASH_ACR_PRFTBE_Msk);
     while (!READ_BIT(FLASH->ACR, FLASH_ACR_PRFTBS_Msk)) {
+        continue;
+    }
+}
+/* ------------------------------------------------------------------------- */
+
+static void rcc_init(void)
+{
+    uint32_t tickstart;
+
+    /* Включить HSE */
+    SET_BIT(RCC->CR, RCC_CR_HSEON_Msk);
+
+    tickstart = systick;
+    while (!READ_BIT(RCC->CR, RCC_CR_HSERDY_Msk)) {
+        if (systick - tickstart > RCC_HSERDY_TIMEOUT) {
+            error();
+        }
+    }
+
+    /* Включить CSS HSE */
+    SET_BIT(RCC->CR, RCC_CR_CSSON_Msk);
+
+    /* Выключить PLL перед настройкой */
+    CLEAR_BIT(RCC->CR, RCC_CR_PLLON_Msk);
+
+    /*
+     * Источник тактирования PLL = HSE (8MHz)
+     * Делитель PLL HSE = /1 (8MHz / 1 = 8MHz)
+     * Множитель PLL = x9 (8MHz * 9 = 72MHz)
+     */
+    MODIFY_REG(RCC->CFGR,
+               RCC_CFGR_PLLXTPRE_Msk
+             | RCC_CFGR_PLLMULL_Msk,
+               RCC_CFGR_PLLSRC_Msk
+             | 0x07 << RCC_CFGR_PLLMULL_Pos);
+
+    /* Включить PLL */
+    SET_BIT(RCC->CR, RCC_CR_PLLON_Msk);
+    while (!READ_BIT(RCC->CR, RCC_CR_PLLRDY_Msk)) {
+        continue;
+    }
+
+    /*
+     * Делитель AHB = /1 (72MHz / 1 = 72MHz)
+     * Делитель APB1 = /2 (72MHz / 2 = 72MHz)
+     * Делитель APB2 = /1 (72MHz / 1 = 72MHz)
+     * Делитель ADC = /6 (72MHz / 6 = 12MHz)
+     * Делитель USB = /1.5 (72MHz / 1.5 = 48MHz)
+     */
+    MODIFY_REG(RCC->CFGR,
+               RCC_CFGR_HPRE_Msk
+             | RCC_CFGR_PPRE1_Msk
+             | RCC_CFGR_PPRE2_Msk
+             | RCC_CFGR_ADCPRE_Msk
+             | RCC_CFGR_USBPRE_Msk,
+               0x04 << RCC_CFGR_PPRE1_Pos
+             | 0x02 << RCC_CFGR_ADCPRE_Pos);
+
+    /* Источник тактирования CPU = PLL */
+    MODIFY_REG(RCC->CFGR,
+               RCC_CFGR_SW_Msk,
+               0x02 << RCC_CFGR_SW_Pos);
+    while (READ_BIT(RCC->CFGR, RCC_CFGR_SWS_Msk) !=
+            0x02 << RCC_CFGR_SWS_Pos) {
         continue;
     }
 }
